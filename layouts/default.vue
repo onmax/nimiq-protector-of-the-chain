6164cfe2b96ce65a2ea4ef11f859a125f5291a8e
Maxi <maximogarciamtnez@gmail.com>
2024-05-30 21:15:32 +0200

    link

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,7 @@ const toggleDark = () => colorMode.value = colorMode.value === 'light' ? 'dark' 
   <header flex="~ gap-32 col lg:row items-center" max-h-screen px-32 py-40 max-w-700 mx-auto>
     <!-- Change the color of the logo for dark mode! -->
     <div i-nimiq:logos-nimiq-horizontal dark:i-nimiq:logos-nimiq-white-horizontal text-90 />
+    <a href="https://github.com/onmax/nimiq-protector-of-the-chain" target="_blank">Github</a>
   </header>
 
   <main max-w-700 mx-auto pt-64 pb-92 flex="~ col items-center" text-center>
